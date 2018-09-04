b7fe13511f8399445d012670153a37f7665cd5ef
kiranreddy75 <saikirangagganeoalli@gmail.com>
2018-09-04 04:39:37 -0700

    first commit

---
+++ AngularForms/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue', 'MEAN Stack',
+              'MERN Stack'];
+
+topicHasError = true ;
+submitted = false;
+errorMsg = '';
+
+  userModel = new User('Sai Kiran', 'Saikiran75@gmail.com', 9194003455, 'default',
+  'morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService){}
+
+  validateTopic(value){
+    if (value ==='default')
+    {
+      this.topicHasError = true;
+    } else
+    {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Success!', data),
+      error => this.errorMsg = error.statusText
+    )
+  }
+}
